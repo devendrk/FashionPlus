e9a164e5d0ae0ebbb134b8d1029b2da71a1bc2da
devendrk <katwaldevendra@hotmail.com>
2020-04-06 12:51:03 +0300

    router, history push

--- src/components/menu-items/menu-items.components.jsx
+++ src/components/menu-items/menu-items.components.jsx
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 import "./menu-items.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  console.log("history", history, match);
   return (
     <div
       className={`${size} menu-item`}
