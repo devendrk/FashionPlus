0e8b16fd46a838a63093e7f48151415ffa73d60d
devendrk <katwaldevendra@hotmail.com>
2020-04-06 16:42:00 +0300

    shop page with collection item and collection preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component'
-import './app.css'
-
-const HatsPage = (props) => (
-  <div>
-    <h1>Hats PAGE</h1>
-  </div>
-)
+import HomePage from "./pages/homepage/homepage.component";
+import ShopePage from "./pages/shop/shop-page.component";
+import "./app.css";
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopePage} />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
