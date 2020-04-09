14ca342f0e06c03ababda7d0b4c44ef21e5553ed
devendrk <katwaldevendra@hotmail.com>
2020-04-10 00:27:46 +0300

    cart reducer, action button

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## FashionPlus
+
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -2,12 +2,15 @@ import cartTypes from "./cartTypes";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+    case cartTypes.ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
 
     default:
       return state;
