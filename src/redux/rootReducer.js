f6cf016590ed3b75a8890d631203737574146d84
devendrk <katwaldevendra@hotmail.com>
2020-04-12 10:30:23 +0300

    director redux init

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user/userReducer";
 import CartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directory.reducer";
 
 export default combineReducers({
   user: userReducer,
   cart: CartReducer,
+  directoryReducer,
 });
